c29874df8689834da2899a742837e1c5705e8e7a
Will Nielsen <wjnielsen@protonmail.com>
2020-07-05 11:07:24 -0500

    * Added docker-compose.yml setting up 	* redis-server 	* node-app * configured index.js to use `redis-server` as the redisClient host.

--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const redis = require('redis');
 
 const app = express();
-const client = redis.createClient();
+const client = redis.createClient({
+  /**
+   * When using docker we don't use the regular hostname or url. Instead,
+   * we can use the name of the docker image. In this case, `redis-server` as
+   * defined in the docker-compose.yml file. The Redis client makes a good faith
+   * effort to connect to a host named `redis-server`, docker will redirect
+   * when it sees the hostname that matches the container specified with a
+   * matching name and connect the two.
+   */
+  host: 'redis-server',
+  port: 6379, // default redis port
+});
 client.set('visits', 0);
 
 app.get('/', (req, res) => {
